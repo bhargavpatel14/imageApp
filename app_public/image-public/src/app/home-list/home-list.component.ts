8374f893d461d55f85e73dc7f64779ecdc2df5a1
ParthDonga13 <80049125+ParthDonga13@users.noreply.github.com>
2021-11-02 02:11:15 -0400

    Changes in .ts files
    Changs in component.ts files

--- app_public/image-public/src/app/home-list/home-list.component.ts
+++ app_public/image-public/src/app/home-list/home-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../image';
+import { ImageDataService } from '../image.service';
 
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
-  styleUrls: ['./home-list.component.css']
+  styleUrls: ['./home-list.component.css'],
+  providers: [ImageDataService]
 })
 export class HomeListComponent implements OnInit {
 
-  constructor() { }
+  images: Image[];
+  constructor(private imageService: ImageDataService ) { }
+
+  private getImages() : void {
+    this.imageService
+      .getImages()
+      .then((images: Image[]) => {
+        this.images = images.map(image => {
+          return image;
+        });
+      });
+  }
 
   ngOnInit(): void {
+    this.getImages();
   }
 
 }
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,1 +1,18 @@
 // test
+var express = require('express');
+var router = express.Router();
+var ctrlHome = require("../controllers/home");
+// var ctrlAbout = require("../controllers/about");
+var ctrlimage = require("../controllers/image");
+
+/* GET home page. */
+router.get('/', ctrlHome.index);
+// router.get('/about', ctrlAbout.aboutus);
+router.get('/list', ctrlimage.mobiles);
+// router.get('/mobile-info/:mobileid', ctrlimage.mobileInfo);
+// router.get('/display', ctrlimage.mobile);
+// router.route('/new')
+//     .get(ctrlimage.addNewMobile)
+//     .post(ctrlimage.doAddNewMobile);
+
+module.exports = router;
