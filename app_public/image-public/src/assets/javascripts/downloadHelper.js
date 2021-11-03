225e2bf5b27934138a20dc8201d9a3ccfb338c66
TejasN <tj.tejas77@gmail.com>
2021-11-03 14:32:15 +0530

    js improvements

--- app_public/image-public/src/assets/javascripts/downloadHelper.js
+++ app_public/image-public/src/assets/javascripts/downloadHelper.js
@@ -1,24 +1,10 @@
-const getFileName = (str) => {
-    try{
-        return str.substring(str.lastIndexOf('/') + 1);
-    }catch(err){
-        console.log("Error in getFileName ", err);
-    }
-};
-
 if(!!document.querySelector('.dwn-btn')){
     console.log("downloadHelper loaded");
     const downBtns = document.querySelectorAll('.dwn-btn');
     downBtns.forEach(elem => {
         elem.addEventListener('click', () => {
-            let imgPath;
-            if(!!document.querySelector('.detail-wrap')){
-                imgPath = document.querySelector('.img-wrap img').src;
-            }
-            else{
-                imgPath = elem.parentElement.previousSibling.previousSibling.previousSibling.previousSibling.firstElementChild.src;
-            }
-            let fileName = getFileName(imgPath);
+            let imgPath = "assets/images/" + elem.getAttribute('img-name');
+            let fileName = elem.getAttribute('img-name');
             saveAs(imgPath, fileName);
         });      
     });
